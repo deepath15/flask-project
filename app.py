2cf38eb5bead9ca52d790d8f76b106f07cc31863
deepath15 <deepath1506@gmail.com>
2023-11-24 20:23:36 +0530

    sign up changed

--- app.py
+++ app.py
@@ -17,7 +17,9 @@ def login():
             return render_template('home.html')
             
     return render_template("index.html")
-
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
