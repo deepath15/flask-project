49a5cc659132cf00888d31caa307e477a4478519
jahangeer7704 <jahangeer7704@gmail.com>
2023-11-24 20:54:54 +0530

    minor

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, redirect, render_template, url_for,flash
-app = Flask(__name__,template_folder="html",static_folder="style")
+app = Flask(__name__,template_folder="html",static_folder="static")
 
 @app.route("/", methods=["post", "get"])
 def login():
@@ -17,7 +17,7 @@ def login():
             return render_template('home.html')
             
     return render_template("index.html")
-@app.route('/signup')
+@app.route('/signup', methods=["get"])
 def signup():
     return render_template('signup.html')
 
